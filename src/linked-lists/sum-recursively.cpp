3e287fcba9169b68087906a6ff74e8faf1b41180
Allen Joseph <allen.joseph.v.a@gmail.com>
2021-05-19 17:43:52 -0500

    Ãfeat(linked-lists): add linked lists exercises

---
+++ src/linked-lists/sum-recursively.cpp
@@ -0,0 +1,399 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//Clase Padre: Lista
+template <typename E>
+class Lista
+{
+private:
+public:
+    //Constructor
+    Lista() {}
+    //Destructor
+    ~Lista() {}
+
+    //Limpiar la lista: dejarla vacia la lista
+    virtual void limpiar() = 0;
+    //Insertar un elemento donde se encuentra el puntero "actual"
+    virtual void insertar(E elemento) = 0;
+    //Agregar un elemento a la lista (al final)
+    virtual void agregar(E elemento) = 0;
+    //Libera de la memoria y retorna el elemento "actual"
+    virtual E eliminar() = 0;
+    //Mover "actual" al inicio de la lista
+    virtual void moverAInicio() = 0;
+    //Mover "actual" al final de la lista
+    // primer espacio disponible
+    virtual void moverAlFinal() = 0;
+    //Mover "actual" a la posicion anterior (izquierda)
+    virtual void anterior() = 0;
+    //Mover "actual" a la posicion posterior (derecha)
+    virtual void siguiente() = 0;
+    //Retornar le numero de elementos de la lista
+    virtual int longitud() = 0;
+    //Retornar la posicion del elemento "actual"
+    virtual int posicionActual() = 0;
+    //"Actual" se mueve a la posicion "pos"
+    virtual void moverAPosicion(int pos) = 0;
+    //Retorna el valor que estaba en la posicion "actual"
+    virtual E getValor() = 0;
+    //Modificar el valor en la posicion "actual"
+    virtual void setValor(E elemento) = 0;
+};
+
+//Clase Hija: ListaArreglo
+template <typename E>
+class ListaArreglo : public Lista<E>
+{
+private:
+    //Arreglo de elementos
+    E *arreglo;
+    //Tamanho fijo
+    int tamMax;
+    //Tamanho de la lista
+    int tamLista;
+    //Indice "actual"
+    int actual;
+
+public:
+    //Constructor
+    ListaArreglo(int tamMax = 100)
+    {
+        this->tamMax = tamMax;
+        this->tamLista = this->actual = 0;
+        this->arreglo = new E[this->tamMax];
+    }
+
+    //Destructor
+    ~ListaArreglo()
+    {
+        delete[] this->arreglo;
+    }
+
+    //Mover "actual" al inicio de la lista
+    void moverAInicio()
+    {
+        this->actual = 0;
+    }
+
+    //Mover "actual" al final de la lista:
+    // primer espacio disponible
+    void moverAlFinal()
+    {
+        this->actual = this->tamLista;
+    }
+
+    //Mover "actual" a la posicion posterior (derecha)
+    void siguiente()
+    {
+        //Sentinela
+        if (this->actual < this->tamLista)
+            this->actual++;
+    }
+
+    //Mover "actual" a la posicion anterior (izquierda)
+    void anterior()
+    {
+        //Sentinela
+        if (this->actual > 0)
+            this->actual--;
+    }
+
+    //Agregar un elemento a la lista (al final)
+    void agregar(E elemento)
+    {
+        this->arreglo[this->tamLista++] = elemento;
+    }
+
+    //Retornar le numero de elementos de la lista
+    int longitud()
+    {
+        return this->tamLista;
+    }
+
+    //Retornar la posicion del elemento "actual"
+    int posicionActual()
+    {
+        return this->actual;
+    }
+
+    //"Actual" se mueve a la posicion "pos"
+    void moverAPosicion(int pos)
+    {
+        //Sentinela
+        if (pos >= 0 && pos < this->tamLista)
+            this->actual = pos;
+    }
+
+    //Retorna el valor que estaba en la posicion "actual"
+    E getValor()
+    {
+        return this->arreglo[this->actual];
+    }
+
+    //Modificar el valor en la posicion "actual"
+    void setValor(E elemento)
+    {
+        this->arreglo[this->actual] = elemento;
+    }
+
+    //Limpiar la lista: dejarla vacia la lista
+    void limpiar()
+    {
+        delete[] this->arreglo;
+        this->tamLista = this->actual = 0;
+        this->arreglo = new E[this->tamMax];
+    }
+
+    //Insertar un elemento donde se encuentra el puntero "actual"
+    void insertar(E elemento)
+    {
+        for (int i = this->tamLista; i > this->actual; i--)
+            this->arreglo[i] = this->arreglo[i - 1];
+        this->arreglo[this->actual] = elemento;
+        this->tamLista++;
+    }
+
+    //Eliminar y retornar el elemento "actual"
+    E eliminar()
+    {
+        E eliminado = this->arreglo[this->actual];
+        for (int i = this->actual; i < this->tamLista; ++i)
+        {
+            this->arreglo[i] = this->arreglo[i + 1];
+        }
+        this->tamLista--;
+        return eliminado;
+    }
+};
+
+//Lista Enlazada
+//Nodo
+template <typename E>
+class Nodo
+{
+public:
+    E elemento;      //Valor para este nodo
+    Nodo *siguiente; //Puntero al siguiente nodo de la lista
+
+    //Constructor
+    Nodo(E elem, Nodo *proximo = NULL)
+    {
+        this->elemento = elem;
+        this->siguiente = proximo;
+    }
+    Nodo(Nodo *proximo = NULL)
+    {
+        this->siguiente = proximo;
+    }
+};
+
+//Clase hija: ListaEnlazada
+template <typename E>
+class ListaEnlazada : public Lista<E>
+{
+private:
+    Nodo<E *> *cabeza; //puntero al primer elemento
+    Nodo<E *> *cola;   //puntero al ultimo elemento
+    Nodo<E *> *actual; //puntero al elemento actual
+    int cantidad;      //tamanho de la lista
+
+    void inicializar()
+    {
+        this->cantidad = 0;
+        this->cola = this->actual = this->cabeza = new Nodo<E *>;
+    }
+
+    void eliminarTodo()
+    {
+        while (this->cabeza != NULL)
+        {
+            this->actual = this->cabeza;
+            this->cabeza = this->cabeza->siguiente;
+            delete actual;
+        }
+    }
+
+public:
+    //Constructor
+    ListaEnlazada()
+    {
+        inicializar();
+    }
+
+    //Destructor
+    ~ListaEnlazada()
+    {
+        eliminarTodo();
+    }
+
+    //Limpiar la lista: dejarla vacia la lista
+    void limpiar()
+    {
+        eliminarTodo();
+        inicializar();
+    }
+
+    //Insertar un elemento donde se encuentra el puntero "actual"
+    void insertar(E elemento)
+    {
+        this->actual->siguiente = new Nodo<E *>(new E(elemento), this->actual->siguiente);
+        if (this->cola == this->actual)
+            this->cola = this->actual->siguiente;
+        this->cantidad++;
+    }
+
+    //Agregar un elemento a la lista (al final)
+    void agregar(E elemento)
+    {
+        this->cola = this->cola->siguiente = new Nodo<E *>(new E(elemento), NULL);
+        this->cantidad++;
+    }
+
+    //Mover "actual" al inicio de la lista
+    void moverAInicio()
+    {
+        this->actual = this->cabeza;
+    }
+
+    //Mover "actual" al final de la lista
+    // primer espacio disponible
+    void moverAlFinal()
+    {
+        this->actual = this->cola;
+    }
+
+    //Mover "actual" a la posicion posterior (derecha)
+    void siguiente()
+    {
+        if (this->actual != this->cola)
+            this->actual = this->actual->siguiente;
+    }
+
+    //Retornar le numero de elementos de la lista
+    int longitud()
+    {
+        return this->cantidad;
+    }
+
+    //Retorna el valor que estaba en la posicion "actual"
+    E getValor()
+    {
+        if (this->actual->siguiente == NULL)
+        {
+            cout << "Valor no existe" << endl;
+            return -1;
+        }
+        return *this->actual->siguiente->elemento;
+    }
+
+    //Modificar el valor en la posicion "actual"
+    void setValor(E elemento)
+    {
+        if (this->actual->siguiente == NULL)
+            cout << "Valor no existe";
+        else
+            *this->actual->siguiente->elemento = elemento;
+    }
+
+    //Mover "actual" a la posicion anterior (izquierda)
+    void anterior()
+    {
+        Nodo<E *> *temp = cabeza;
+        Nodo<E *> *tempActual = actual;
+
+        moverAInicio();
+        // int i;
+        for (int i = 0; tempActual != temp; ++i)
+        {
+            temp = temp->siguiente;
+            if (tempActual != temp)
+            {
+                siguiente();
+            }
+        }
+    }
+
+    //Retornar la posicion del elemento "actual"
+    int posicionActual()
+    {
+        Nodo<E *> *temp = cabeza;
+        int i;
+        for (i = 0; actual != temp; ++i)
+        {
+            temp = temp->siguiente;
+        }
+        return i;
+    }
+
+    //Libera de la memoria y retorna el elemento "actual"
+    E eliminar()
+    {
+        Nodo<E *> *tempActual = actual;
+        Nodo<E *> *temp = cabeza;
+
+        moverAInicio();
+        for (int i = 0; tempActual != temp; ++i)
+        {
+            temp = temp->siguiente;
+            if (temp == tempActual && temp != cola)
+            {
+                actual->siguiente = actual->siguiente->siguiente;
+                this->cantidad--;
+            }
+            else
+            {
+                siguiente();
+            }
+        }
+
+        return *tempActual->elemento;
+    }
+
+    //"Actual" se mueve a la posicion "pos"
+    void moverAPosicion(int pos)
+    {
+    }
+
+    Nodo<E *> *primerNodo()
+    {
+        return cabeza;
+    }
+};
+
+int suma(int sum, Nodo<int *> *node)
+{
+    if (node->siguiente != NULL)
+    {
+        sum = sum + *node->siguiente->elemento;
+        return suma(sum, node->siguiente);
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int len;
+    cout << "Enter the number of elements: ";
+    cin >> len;
+
+    ListaEnlazada<int> *list = new ListaEnlazada<int>();
+
+    int element;
+    for (int i = 0; i < len; i++)
+    {
+        cout << "Enter value " << i + 1 << ": ";
+        cin >> element;
+        list->agregar(element);
+    }
+
+    int sum = suma(0, list->primerNodo());
+
+    cout << "The result is: ";
+    cout << sum << endl;
+
+    return 0;
+}
